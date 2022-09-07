39231299fed827b58133c2b89a7dc4cf7659a58d
ValloonSweet <valloon91105@gmail.com>
2022-09-07 14:46:26 +0800

    add edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,28 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def edit
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    # here :id means path variable
+    @article = Article.find(params[:id])
+  end
+
+
   private
-    def article_params
+  def article_params
       params.require(:article).permit(:title, :description)
     end
 
