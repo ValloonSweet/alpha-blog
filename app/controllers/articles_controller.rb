d5a37eb5aabb7bf82417712ad594770f700c803c
ValloonSweet <valloon91105@gmail.com>
2022-09-07 15:34:25 +0800

    partial, before actioin, delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -19,11 +21,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -33,13 +33,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    # here :id means path variable
-    @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article.destroy
+
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to article_path, status: :see_other
+  end
 
   private
-  def article_params
+    def set_article
+      # here :id means path variable
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
       params.require(:article).permit(:title, :description)
     end
 
